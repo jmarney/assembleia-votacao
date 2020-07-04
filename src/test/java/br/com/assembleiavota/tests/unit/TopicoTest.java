fda234f2f79016668c28c7065202b1e40f0934cb
Joao Marney <joao.marney@dbccompany.com.br>
2020-07-04 13:37:55 -0300

    Add anotation em TopicoTest

--- src/test/java/br/com/assembleiavota/tests/unit/TopicoTest.java
+++ src/test/java/br/com/assembleiavota/tests/unit/TopicoTest.java
@@ -3,10 +3,15 @@ package br.com.assembleiavota.tests.unit;
 import br.com.assembleiavota.model.Topico;
 import br.com.assembleiavota.repository.TopicoRepository;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(SpringRunner.class)
+@DataJpaTest
 public class TopicoTest {
 
     @Autowired
